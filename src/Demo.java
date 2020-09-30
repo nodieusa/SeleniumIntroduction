8642c119c543e8f6fbf1d6b25223ffb473e31cde
Ederson Barros <ederson.barros@aa.com>
2020-09-30 11:58:04 -0500

    second commit

--- src/Demo.java
+++ src/Demo.java
@@ -5,8 +5,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\windows\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver ();
-		
+		driver.get("http://google.com");
 	}
-
 }
