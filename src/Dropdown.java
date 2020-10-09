001cfe751ac85535ccf4ea17c1bf037c76365e50
Ederson Barros <ederson.barros@aa.com>
2020-10-09 14:54:12 -0500

    8-59

--- src/Dropdown.java
+++ src/Dropdown.java
@@ -1,24 +1,30 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class Dropdown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "c:\\Windows\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://spicejet.com/");
-//		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(3000);
 		driver.findElement(By.cssSelector("#divpaxinfo")).click();
+		Thread.sleep(2000);
 		Select sAdults = new Select(driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_Adult")));
-		sAdults.selectByValue("2");
-
+		sAdults.selectByValue("5");
+		Assert.assertEquals(driver.findElement(By.cssSelector("#divpaxinfo")).getText(), "5 Adult");
+		
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		
 		Select s = new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
 		s.selectByValue("USD");
-		
+		Thread.sleep(3000);
 		driver.quit();
 	}
 
