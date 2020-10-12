dc14f9548d03bbbd025bae3954835c597ec03047
Ederson Barros <ederson.barros@aa.com>
2020-10-12 16:21:35 -0500

    8-62 in progress

--- src/e2e.java
+++ src/e2e.java
@@ -10,14 +10,17 @@ public class e2e {
 		System.setProperty("webdriver.chrome.driver", "c:\\Windows\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://spicejet.com/");
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1")).click();
+/*		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1")).click();
 		System.out.println("print: " + driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).getAttribute("value"));
 		driver.findElement(By.xpath("//a[@value='BLR']")).click();
-/*		Select sDeparture = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_originStation1")));
+*/
+		Select sDeparture = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_originStation1")));
 		sDeparture.selectByValue("BLR");
-		driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1")).click();
-		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
-*/		
+//		driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1")).click();
+//		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+//		Select sArrival = new Select(driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")));
+		Select sArrival = new Select(driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1']")));
+		sArrival.selectByValue("MAA");
 		Thread.sleep(3000);
 		driver.quit();
 	}
